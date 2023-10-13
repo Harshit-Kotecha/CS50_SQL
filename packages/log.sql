ec7900c99e650c976ad0695ac64f6355bcaa5418
harshit-kotecha <harshitkotecha2000@gmail.com>
2023-10-14 02:38:26 +0530

    Add final solutions for "packages" problem set

--- packages/log.sql
+++ packages/log.sql
@@ -32,6 +32,59 @@ WHERE "id" = (
 );
 
 -- *** The Devious Delivery ***
+SELECT * FROM "packages"
+WHERE "from_address_id" IS NULL;
+
+SELECT * FROM "scans"
+WHERE "package_id" = (
+    SELECT "id" FROM "packages"
+    WHERE "from_address_id" IS NULL
+);
+
+SELECT * FROM "addresses"
+WHERE "id" = (
+    SELECT "address_id" FROM "scans"
+    WHERE "package_id" = (
+        SELECT "id" FROM "packages"
+        WHERE "from_address_id" IS NULL
+    ) AND "action" = 'Drop'
+);
 
 -- *** The Forgotten Gift ***
+SELECT * FROM "packages"
+WHERE "from_address_id" = (
+    SELECT "id" FROM "addresses"
+    WHERE "address" = '109 Tileston Street'
+) AND "to_address_id" = (
+    SELECT "id" FROM "addresses"
+    WHERE "address" = '728 Maple Place'
+);
 
+SELECT * FROM "scans"
+WHERE "package_id" = (
+    SELECT "id" FROM "packages"
+    WHERE "from_address_id" = (
+        SELECT "id" FROM "addresses"
+        WHERE "address" = '109 Tileston Street'
+    ) AND "to_address_id" = (
+        SELECT "id" FROM "addresses"
+        WHERE "address" = '728 Maple Place'
+    )
+);
+
+SELECT * FROM "drivers"
+WHERE "id" = (
+    SELECT "driver_id" FROM "scans"
+    WHERE "package_id" = (
+        SELECT "id" FROM "packages"
+        WHERE "from_address_id" = (
+            SELECT "id" FROM "addresses"
+            WHERE "address" = '109 Tileston Street'
+        ) AND "to_address_id" = (
+            SELECT "id" FROM "addresses"
+            WHERE "address" = '728 Maple Place'
+    )
+    )
+    ORDER BY "timestamp" DESC
+    LIMIT 1
+);
